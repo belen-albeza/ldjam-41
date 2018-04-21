cdf66911cee0d222492e5225cbe9b6faae27714e
Belén Albeza <belen.albeza@gmail.com>
2018-04-21 12:48:23 +0200

    initial scaffolding

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var PlayScene = require('./play_scene.js');
+
+var BootScene = {
+  init: function () {
+    // set scale mode
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.pageAlignHorizontally = true;
+    this.game.scale.pageAlignVertically = true;
+  },
+
+  preload: function () {
+    // load here assets required for the loading screen
+    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+  },
+
+  create: function () {
+    this.game.state.start('preloader');
+  }
+};
+
+
+var PreloaderScene = {
+  preload: function () {
+    this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
+    this.loadingBar.anchor.setTo(0, 0.5);
+    this.load.setPreloadSprite(this.loadingBar);
+
+    // TODO: load here the assets for the game
+    this.game.load.image('logo', 'images/phaser.png');
+  },
+
+  create: function () {
+    this.game.state.start('play');
+  }
+};
+
+
+window.onload = function () {
+  var game = new Phaser.Game(768, 768, Phaser.AUTO, 'game');
+
+  game.state.add('boot', BootScene);
+  game.state.add('preloader', PreloaderScene);
+  game.state.add('play', PlayScene);
+
+  game.state.start('boot');
+};
